0b2b591cffd7fd66fec52b03990cfbfe438b13bd
MD Mazbaul Islam <mazbaul20@gmail.com>
2022-11-05 16:44:24 +0600

    update stack

--- Algorithm/stack/stack.c
+++ Algorithm/stack/stack.c
@@ -1,4 +1,44 @@
 #include<stdio.h>
+#define CAPACITY 3
+int stack[CAPACITY];
+int top = -1;
+
+void push(int x){
+    if(top<CAPACITY-1){
+        top+=1;//top=top+1;
+        stack[top]=x;
+        printf("Successful added item: %d\n",x);
+    }else{
+        printf("Exception! no spaces\n");
+    }
+}
+int pop(){
+    if(top>=0){
+        int val = stack[top];
+        top-=1;
+        return val;
+    }else{
+        printf("Exception from pop! Empty stack\n");
+    }
+    return -1;
+}
+int peak(){
+    if(top>=0){
+        return stack[top];
+    }else{
+        printf("Exception from peak! Empty stack\n");
+    }
+    return -1;
+}
 int main(){
     printf("Implementation stack in c\n");
+    peak();
+    push(10);
+    push(20);
+    push(30);
+    printf("Pop of stack:%d\n",pop());
+    push(40);
+    printf("Top of stack: %d\n",peak());
+
+    return 0;
 }
--- Practice any problem/practice 2/practice 2.c
+++ Practice any problem/practice 2/practice 2.c
@@ -2,40 +2,34 @@
 #define CAPACITY 3
 int stack[CAPACITY];
 int top = -1;
+
 void push(int x){
-    if(top <CAPACITY-1){
-        top+=1;
+    if(top<CAPACITY-1){
+        top+=1;//top=top+1;
         stack[top]=x;
         printf("Successful added item: %d\n",x);
     }else{
         printf("Exception! no spaces\n");
     }
 }
-
 int pop(){
-    if(top>=0){
-        int val = stack[top];
-        top -=1;
-        return val;
-    }
-    printf("Exception from pop! Empty stack\n");
-    return -1;
+    //return -1;
 }
 int peak(){
     if(top>=0){
         return stack[top];
+    }else{
+        printf("Exception from peak! Empty stack\n");
     }
-    printf("Exception from peak! Empty stack\n");
-    return -1;
+    //return -1;
 }
 int main(){
     printf("Implementation stack in c\n");
-    peak();
+    display();
     push(10);
     push(20);
     push(30);
-    printf("Pop of stack: %d\n",pop());
     push(40);
-    printf("Top of stack: %d\n",peak());
+
     return 0;
 }
